bd139b53dd823b6bf98747e316872adaf2317d0f
BrianSipple <bsipple57@gmail.com>
2016-10-28 01:12:36 -0700

    remove custom settings from base index.js

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 module.exports = {
   extends: [
-    'eslint:recommended',
     './rules/best-practices.js',
     './rules/es2015.js',
     './rules/possible-errors.js',
     './rules/strict-mode.js',
     './rules/stylistic-issues.js',
     './rules/variables.js'
-  ],
-
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module',
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true
-    }
-  }
+  ]
 };
